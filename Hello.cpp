e8eee5e28aba60e48253127a8c1088174d8125fc
rbalsa <ramiro.balsa@live.com>
2017-04-04 13:04:06 -0300

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+/*Hello World
+ *Ramiro Balsa
+ *04042017
+ */
+ 
+ #include <iostream> 
+ 
+ int main () {
+ 	std :: cout << "Hello, world! \n";
+ 	
+ 	
+ }
---
+++ Suma de dos números.cpp
@@ -0,0 +1,25 @@
+/*Suma entre dos números
+ *Ramiro Balsa
+ *04042017
+ */
+ 
+ #include <iostream>
+ 
+ int main (){
+ 	int i,j,suma;
+ 	
+ 	std :: cout << "Ingrese un numeros de dos digitos \n " ;
+ 	 		
+ 	std :: cin >> i;
+ 	
+ 	std :: cout << "Ingrese otro numero de dos digitos \n " ;
+ 	
+ 	std :: cin >> j;
+ 	
+ 	suma = i+j;
+ 		
+ 	
+	std :: cout << " El resultado es " << suma;
+
+ return 0;	
+}    
